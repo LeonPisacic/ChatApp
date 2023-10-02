b9ed0d7c6ed7fcd74dd9bc9c5e13da96a1909f89
Leon <leon.pisacic2002@gmail.com>
2023-10-02 14:12:25 +0200

    Commit of simple chat app using Java/JavaScript

---
+++ chat/src/main/java/com/alibou/chat/chat/ChatController.java
@@ -0,0 +1,28 @@
+package com.alibou.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    // Send message Method
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    // Add username in web socket session
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage user(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+
+        return chatMessage;
+    }
+
+}
